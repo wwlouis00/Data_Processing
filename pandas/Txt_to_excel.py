c9001f78085c06522c765ac302bbb8ef75734aa4
unknown <wwlouis00@gmail.com>
2021-12-09 10:45:03 +0800

    update 2021/12/09

--- pandas/Txt_to_excel.py
+++ pandas/Txt_to_excel.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 df = pd.read_csv('data/2021年12月08日_未刨開.txt',delimiter='\t')
 df.columns = ['Time','index','None', 'CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8']
