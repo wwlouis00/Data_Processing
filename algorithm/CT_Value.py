446ab1dc354e2af8f01cbe744debae5ffa460f9a
unknown <wwlouis00@gmail.com>
2021-12-15 10:14:31 +0800

    remove some files

--- algorithm/CT_Value.py
+++ algorithm/CT_Value.py
@@ -1,67 +1,100 @@
-import time
-import datetime
-from datetime import datetime, timedelta
 import pandas as pd
-import statistics
-import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
-from openpyxl.worksheet.dimensions import ColumnDimension, RowDimension
-import numpy as np
-from openpyxl import load_workbook
-from heapq import nsmallest
-#-----------------------------------------------------------------------------------------------------------------------------
-now_output_time = str(datetime.now().strftime('%Y-%m-%d %H-%M-%S'))+"output.xlsx"
-print("-"*200)
-df = pd.read_csv("2021_10_22_16_26_07pos VTM A.csv")
-#插入首行
-df.drop(df.index[0])
-df.columns = ['Time','temp_lid', 'temp_well', 'well_1', 'well_2', 'well_3', 'well_4', 'well_5', 'well_6', 'well_7', 'well_8','well_9', 'well_10', 'well_11', 'well_12', 'well_13', 'well_14', 'well_15', 'well_16']
-print("-"*200)
-print("橫列數"+str(len(df.columns)))
-print("直列數"+str(len(df.index)-1))
-time_1 = datetime.strptime(df.loc[1, 'Time'],"%H:%M:%S")
-time_2 = datetime.strptime(df.loc[len(df.index)-1, 'Time'],"%H:%M:%S")
-time_interval = time_2 - time_1
-print("測試分鐘: "+str(time_interval))
-print("-"*100)
-#-----------------------------------------------------------------------------------------------------------------------------
-time_array = []
-for i in range(0, len(df.index), 1):
-    time_array.append(i)
-well_array = []
-well_var = []
-well_threshold =[]
-start_time = int(input("輸入時段1:    "))
-start_time_2 = int(input("輸入時段2:    "))
-print(start_time *2)
-print(start_time_2 *2)
-for i in range(1,17,1):
-    print("well_"+str(i))
-    well_array.append([df.loc[data, 'well_' + str(i)] for data in range(len(df.index))])
-    del well_array[i-1][0]
-    #平均
-    sum = 0
-    for j in range(7,30,1):
-        sum = sum + int(well_array[i-1][j])
-    avg_well = sum/23
-    print("平均:"+ str(avg_well))
-    var = 0
-    for k in range(7,30,1):
-        var = abs((int(well_array[i-1][k]) - avg_well) / avg_well)
-        well_var.append(var)
-    print(np.std(well_var))
-    threshold= var*10 + avg_well
-    well_threshold.append(threshold)
-    well_cut = []
-    for m in range(0,len(df.index)-1,1):
-        if threshold <= int(well_array[i-1][m]) and  m >= 31:
-            if len(well_cut) == 0:
-                well_cut.append(m)
-                print(well_array[i-1][m])
-                print(m)
-            if len(well_cut) >= 1:
-                break
-    print(well_cut)   
+import os
+from datetime import datetime
 
 
-df.to_excel('CT_Value_' + now_output_time, encoding="utf_8_sig")
+# global variable
+raw_file_path = "2021_10_22_16_26_07pos.csv"
+ifc_file_path = "cali_factor.csv"
+
+def get_accumulation_time():
+    df_time = df_normalization['time']
+    time_ori = datetime.strptime(df_time[0], "%H:%M:%S")
+    time_delta = []
+    for time in df_time:
+        time_now = datetime.strptime(time, "%H:%M:%S")
+        time_delta.append((time_now - time_ori).seconds/60)
+    df_normalization.insert(1, column="accumulation", value=time_delta)
+
+def get_StdDev_and_Avg():
+    StdDev = []
+    Avg = []
+    for i in range(0, 16):
+        df_current_well = df_normalization[f'well_{i+1}']
+        StdDev.append(df_current_well[8:30].std())
+        Avg.append(df_current_well[8:30].mean())
+    return StdDev, Avg
+
+def normalize():
+    for i in range(0, 16):
+        df_current_well = df_raw[f'well_{i+1}']
+        df_current_ifc = df_ifc[f'well{i+1}']
+        baseline = df_current_well[8:30].mean()
+        df_normalization[f'well{i+1}'] = (df_raw[f'well_{i+1}']-baseline)/df_current_ifc[0] # normalized = (IF(t)-IF(b))/IFc
+
+def get_ct_threshold():
+    threshold_value = []
+    StdDev, Avg = get_StdDev_and_Avg()
+    for i in range(0, 16):
+        threshold_value.append(10*StdDev[i] + Avg[i])
+        print(f"Well {i+1}: StdDev is {StdDev[i]}, Avg is {Avg[i]}")
+    return threshold_value
+
+def get_ct_value(threshold_value):
+    Ct_value = []
+    for i in range(0, 16):
+        df_current_well = df_normalization[f'well_{i+1}']
+        df_accumulation = df_normalization['accumulation']
+        print("\n")
+        print(df_current_well)
+        print(f"Threshold value: {threshold_value[i]}")
+        try:
+            for j, row in enumerate(df_current_well):
+                if row >= threshold_value[i]:
+                    print(f"row: {row}")
+                    thres_lower = df_current_well[j-1]
+                    thres_upper = df_current_well[j]                
+                    acc_time_lower = df_accumulation[j-1]
+                    acc_time_upper = df_accumulation[j+1]
+                    
+                    # linear regression
+                    x2 = acc_time_upper
+                    y2 = thres_upper
+                    x1 = acc_time_lower
+                    y1 = thres_lower
+                    y = threshold_value[i]
+                    x = (x2-x1)*(y-y1)/(y2-y1)+x1
+
+                    Ct_value.append(round(x, 2))
+                    print(f"Ct of well_{i+1} is {round(x, 2)}")
+                    break
+
+                # if there is no Ct_value availible
+                elif j == len(df_current_well)-1:
+                    Ct_value.append(99.99)
+                    print("Ct value is not available")
+        except Exception as e:
+            print(e)
+            Ct_value.append(99.99)
+            print("Ct value is not available")
+
+    return Ct_value
+
+def ct_calculation():
+    global df_raw, df_ifc, df_normalization
+    df_raw = pd.read_csv(raw_file_path)
+    df_ifc = pd.read_csv(ifc_file_path)
+    df_normalization = df_raw.copy()
+
+    get_accumulation_time()
+    normalize()
+    # df_normalization.to_csv("./result/normalization.csv", index=False)
+    threshold_value = get_ct_threshold()
+    Ct_value = get_ct_value(threshold_value)
+    return Ct_value
+
+def main():
+    ct_calculation()
+if __name__ == '__main__':
+    main()
+    
