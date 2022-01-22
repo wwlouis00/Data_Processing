5a356608350049f0f4761467ea774d6787c3c8a5
unknown <wwlouis00@gmail.com>
2022-01-22 11:19:45 +0800

    update 2022/1/22

--- pandas/Txt_to_excel.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/2021年12月09日_有刨開.txt',delimiter='\t')
-df.columns = ['Time','index','None', 'CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8']
-print(df)
-df.to_excel('data/2021年12月09日_有刨開.xlsx')
